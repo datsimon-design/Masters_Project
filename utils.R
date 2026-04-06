e008285212dd1153d6295414956d8844642ef680
Simon <simon.gendrisch@gmail.com>
2026-04-06 20:59:54 +0200

    added get_full function

--- utils.R
+++ utils.R
@@ -32,7 +32,7 @@ get_marine <- function(past_days = 1, forecast_days = 0) {
   library(jsonlite)
   
     url_marine <- paste0("https://marine-api.open-meteo.com/v1/marine?latitude=36.1447&longitude=-5.3526&hourly=wave_height,sea_surface_temperature,sea_level_height_msl,ocean_current_velocity,ocean_current_direction,wave_direction,wave_period,wave_peak_period,wind_wave_height,wind_wave_direction&models=best_match&past_days=", past_days,
-  "&forecast_days=", forecast_days, "&minutely_15=ocean_current_velocity,ocean_current_direction,sea_level_height_msl")
+  "&forecast_days=", forecast_days)
   
     red_m <- GET(url_marine)
     
@@ -45,4 +45,44 @@ get_marine <- function(past_days = 1, forecast_days = 0) {
   
   
 }
+
+
+# Pull full dataframe and add time and date column
+get_full <- function(past_days = 1, forecast_days = 0) {
+  library(tidyverse)
+  
+  # Pull weather data
+  url_land <- paste0("https://api.open-meteo.com/v1/forecast?latitude=36.1447&longitude=-5.3526&hourly=temperature_2m,relative_humidity_2m,precipitation,rain,weather_code,visibility,pressure_msl,surface_pressure,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&models=ukmo_seamless&timezone=auto&past_days=",past_days,
+                "&forecast_days=", forecast_days)
+  
+  red <- httr::GET(url_land)
+  
+  df <- jsonlite::fromJSON(httr::content(red, "text", encoding = "UTF-8"))
+  
+  weather_data <<- as.data.frame(df)
+  
+  # Same for marine
+  url_marine <- paste0("https://marine-api.open-meteo.com/v1/marine?latitude=36.1447&longitude=-5.3526&hourly=wave_height,sea_surface_temperature,sea_level_height_msl,ocean_current_velocity,ocean_current_direction,wave_direction,wave_period,wave_peak_period,wind_wave_height,wind_wave_direction&models=best_match&past_days=", past_days,
+                       "&forecast_days=", forecast_days)
+  
+  red_m <- httr::GET(url_marine)
+  
+  df_ma <- jsonlite::fromJSON(httr::content(red_m, "text", encoding = "UTF-8"))
+  
+  marine_data <<- as.data.frame(df_ma)
+  
+  
+  
+  # Join and add date and time
+  full_data <<- full_join(marine_data, weather_data, by = "hourly.time") |>
+    mutate(
+      datetime = lubridate::ymd_hm(hourly.time),
+      date = as.Date(datetime),
+      time = format(datetime, "%H:%M") 
+    )
+  
+  summary(full_data)
+
+}
+
 
